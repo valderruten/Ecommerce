14b75e9b4fe9b9560fab166b60b685e3f8341a64
Carlos Andrés V <valderruten@gmail.com>
2023-01-07 08:15:06 -0500

    first commit

--- src/components/ProductInfo/SliderImg.jsx
+++ src/components/ProductInfo/SliderImg.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./styles/sliderImg.css"
 
-const SliderImg = ({listImgs}) => {
-  console.log(listImgs)
-  return (
-    <div className='slider'>
-      <div className='slider-container'>
-        {
-          
-        }
+const SliderImg = ({ listImgs }) => {
+ 
+  const [indexImg, setIndexImg] = useState(0)
+  
+  const styleContainer={
+    transform:`translateX(calc(100%*-${indexImg}/3))`
+  }
 
-      </div>
-    </div>
-  )
+const handleBack=()=>{
+  if(indexImg-1<0)
+  {
+setIndexImg(2)
+  }else {
+    setIndexImg(indexImg-1)
+  }
+
+}
+const handleNext=()=>{
+  if(indexImg+1>2){
+     setIndexImg(0)
+  }else{
+    setIndexImg(indexImg+1)
+  }
 }
 
+  return (
+  <div className="slider">
+    <button onClick={handleBack} className='slider__back'>&#60;</button>
+    <div style={styleContainer}className='slider__container'>
+      {
+      listImgs?.map(url => (
+      <div className='slider__img-container' key={url}>
+        <img className='slider__img' src={url} alt="" />
+        </div>
+        )
+        )}
+        </div>
+        <button onClick={handleNext} className='slider__next'>&#62;</button>
+        
+        </div>
+        )}
 export default SliderImg
